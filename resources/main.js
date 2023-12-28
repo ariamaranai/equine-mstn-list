09ee35517192779bbdf29e31758b3d77c568516f
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2023-12-29 01:36:10 +0900

    Create main.js

---
+++ resources/main.js
@@ -0,0 +1,9 @@
+addEventListener('wheel',Number.isInteger(devicePixelRatio) ?
+  a => a.preventDefault(document.body.scrollTop += (a.deltaY < 0 ? 0 : 45) - document.body.scrollTop % 45 || -45) :
+  a => a.preventDefault(document.body.scrollTop += a.deltaY < 0 ? -45 : 45), {passive: !1}),
+ondragstart =a=> !1,
+oninput =a=> {
+  let r = $total, s = 0, e;
+  while ((e = document.body.children[r]).className = e.textContent.includes(a.target.value) ? (++s,'p') : 's', --r);  
+  p.textContent = s
+}
