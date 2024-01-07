fe9227207d74a3eb5a13d13a0567f6ae6ae6e65d
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-01-07 17:44:47 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,6 @@
-addEventListener('wheel',Number.isInteger(devicePixelRatio) ?
-  a => a.preventDefault(document.body.scrollTop += (a.deltaY < 0 ? 0 : 45) - document.body.scrollTop % 45 || -45) :
-  a => a.preventDefault(document.body.scrollTop += a.deltaY < 0 ? -45 : 45), {passive: !1}),
+addEventListener('wheel',devicePixelRatio == 1 ?
+  a => a.preventDefault(document.body.scrollTop += (a.deltaY < 0 ? 0 : 41) - document.body.scrollTop % 41 || -41) :
+  a => a.preventDefault(document.body.scrollTop += a.deltaY < 0 ? -41 : 41), {passive: !1}),
 ondragstart =a=> !1,
 oninput =a=> {
   let r = $total, s = 0, e;
