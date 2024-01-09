fd39dfe89214e5f5436cd67b13821b6c0c676355
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-01-09 13:36:14 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -2,26 +2,28 @@ import horses from "./mstn.json";
 
 let total = horses.length;
 let css = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
-let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replace("$total", total);
+let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replaceAll("$total", total);
 let html = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", css).replace("/*js*/", js);
 
-for (let y = 0;;) {
+for (let horse of horses) {
   html += "<p>";
-  for (let row = horses[y], x = 0; x < 4; ++x) {
-    let cell = row[x];
-    let href = cell.href;
-    let mstn = cell.mstn;
-    html += `<a${href && " href=" + href.slice(href[12] == "j" ? -11 : 6)}` +
-            `${mstn ? mstn == "CC" ? " s" : mstn == "CT" ? " a" : " e" : ""}>` +
-            `${cell.name}\u000a${cell.year}-${cell.cnty}` + (x ? "": `-${cell.sex}`);
+  for (let i = 0; i < 4; ++i) {
+    let h = horse[i];
+    let href = h.href;
+    if (href)
+      if (href[12] == "j")
+        href = " href=" + href.slice(-11);
+      else
+        href = " href=//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
+    else
+      href = "";
+    let mstn = h.mstn;
+    mstn = mstn ? mstn == "CC" ? " s" : mstn == "CT" ? " a" : " e" : "";
+    html += `<a${href}${mstn}>${h.name}\u000a${h.year}-${h.cnty}${i ? "" : "-" + h.sex}`;
   }
   html += "</a>";
-  if (++y == total) {
-    html += "</p>by <a href=//ariamaranai.github.io/ id=p>@ariamaranai";
-    break;
-  }
 }
 
+html += "</p>by <a href=//ariamaranai.github.io/ style=display:contents>@ariamaranai";
 Bun.write("../index.htm", html);
-let size = Bun.gzipSync(Buffer.from(html));
-console.log(`size: ${size.length}`);
+console.log(`size: ${Bun.gzipSync(Buffer.from(html)).length}`);
