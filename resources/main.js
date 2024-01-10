ab4a2b58db9c2ed58b5b0f388b6868879dc7ee50
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-01-10 20:28:14 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,13 +1,11 @@
+onload =t=>
+  addEventListener('wheel', Number.isInteger(t = a.nextSibling.getBoundingClientRect().height) ? 
+      n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? t - document.body.scrollTop % t || t : -document.body.scrollTop % t || -t) :
+      n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? t : -t),
+  { passive: !1 }),
+ondragstart =n=> !1,
 oninput =n=> {
   let e = document.body.children, r = $total, t = 0;
   while (e[r].className = e[r].textContent.includes(n.target.value) ? '' : (++t,'p'), --r);
   p.textContent = t
-},
-onload =t=> {
-  Number.isInteger(t = a.nextSibling.getBoundingClientRect().height) && addEventListener('wheel', n => {
-    let e = document.body.scrollTop;
-    n.preventDefault(),
-    document.body.scrollTop -= n.deltaY < 0 ? e % t || t : e % t - t || -t
-  }, { passive: !1 })
-},
-ondragstart =n=> !1
+}
