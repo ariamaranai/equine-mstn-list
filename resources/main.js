0cdfdcb6f5a5f5a9e06fddfacf7304d7f9458e27
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-04-22 19:26:52 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,8 @@
-addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 40 - document.body.scrollTop % 40 || 40 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 40 || -40), { passive: !1 }),
+addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 24 - document.body.scrollTop % 24 || 24 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 24 || -24), { passive: !1 }),
 ondragstart =n=> !1,
-oninput =n=> {
+a.oninput =n=> {
   let e = document.body.children, r = $total, t = 0;
-  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 'a', --r);
-  p.textContent = t
-}
+  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 's', --r);
+  s.textContent = t
+},
+b.onchange =n=> open('//ariamaranai.github.io/equine-bmi-list/' + ['','wt.htm','bmi.htm'][b.selectedIndex])
