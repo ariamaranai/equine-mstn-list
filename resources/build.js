a1321e2a894c74b19427e794b4234acc2da97728
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-05 10:53:53 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -4,26 +4,37 @@ let total = horses.length;
 let css = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
 let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replaceAll("$total", total);
 let html = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", css).replace("/*js*/", js);
+let hrefsForDebug = [];
+let namesForDebug = [];
 
 for (let i = 0; i < total; ++i) {
   html += "<p>";
   for (let j = 0; j < 4; ++j) {
     let { name, year, cnty, sex, tb, href, mstn } = horses[i][j];
     if (href) {
+      // Debug
+      let hrefsIdx = hrefsForDebug.indexOf(href);
+      if (hrefsIdx < 0) {
+        hrefsForDebug.push(href);
+        namesForDebug.push(name);
+      } else if (namesForDebug[hrefsIdx] != name) {
+        console.log(horses[i][j]);
+      }
+
       let host = href.slice(8, href.indexOf("/", 10));
       if (host == "www.jbis.or.jp")
         href = " href=" + href.slice(-11);
       else if (host == "www.pedigreequery.com")
-        href = " href=//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
+        href = " href=//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v.length < 3 ?  v : v[0].toUpperCase() + v.slice(1)).join("+");
       else if (host == "www.allbreedpedigree.com")
         href = " href=//www.allbreedpedigree.com/" + href.slice(33).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
       else
         href = "href=" + href.slice(6);
-    } else {
-      href = "";
+
     }
     mstn = mstn ? mstn == "CC" ? " s" : mstn == "CT" ? " a" : " e" : "";
     html += `<a${href}${mstn}>${name}\u000a${year ? year : "?"}-${cnty ? cnty : "?"}${j ? "" : "-" + sex}${tb ? "" : " *"}`;
+
   }
   html += "</a>"
 }
