14ddba53ac10053083209bd7879becb58cccdea4
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-06 12:47:34 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -4,11 +4,16 @@ let total = horses.length;
 let css = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
 let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replaceAll("$total", total);
 let html = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", css).replace("/*js*/", js);
+let mainNameForDebug = [];
 let hrefsForDebug = [];
 let namesForDebug = [];
 
 for (let i = 0; i < total; ++i) {
   html += "<p>";
+  // Debug
+  let mainName = horses[i][0].name;
+  mainName != "-" && mainNameForDebug.includes(mainName) ? console.log(mainName) : mainNameForDebug.push(mainName);
+
   for (let j = 0; j < 4; ++j) {
     let { name, year, cnty, sex, tb, href, mstn } = horses[i][j];
     if (href) {
@@ -18,7 +23,7 @@ for (let i = 0; i < total; ++i) {
         hrefsForDebug.push(href);
         namesForDebug.push(name);
       } else if (namesForDebug[hrefsIdx] != name) {
-        console.log(horses[i][j]);
+        console.log(horses[i]);
       }
 
       let host = href.slice(8, href.indexOf("/", 10));
@@ -27,10 +32,14 @@ for (let i = 0; i < total; ++i) {
       else if (host == "www.pedigreequery.com")
         href = " href=//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v.length < 3 ?  v : v[0].toUpperCase() + v.slice(1)).join("+");
       else if (host == "www.allbreedpedigree.com")
-        href = " href=//www.allbreedpedigree.com/" + href.slice(33).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
+        href = " href=//www.allbreedpedigree.com/" + href.slice(33).split("+").map(v => v.length < 3 ? v : v[0].toUpperCase() + v.slice(1)).join("+");
+      else if (host == "www.horsetelex.com") {
+        let idx = href.indexOf("/", 44) + 1;
+        href = " href=" + href.slice(6, idx) + href.slice(idx).split("-").map(v => v.length < 3 ? v : v[0].toUpperCase() + v.slice(1)).join("-");
+      }  else if (host == "sporthorse-data.com") 
+        href = " href=//sporthorse-data.com/pedigree/" + href.slice(37).split("-").map(v => v.length < 3 ? v : v[0].toUpperCase() + v.slice(1)).join("-");
       else
-        href = "href=" + href.slice(6);
-
+        href = " href=" + href.slice(6);
     }
     mstn = mstn ? mstn == "CC" ? " s" : mstn == "CT" ? " a" : " e" : "";
     html += `<a${href}${mstn}>${name}\u000a${year ? year : "?"}-${cnty ? cnty : "?"}${j ? "" : "-" + sex}${tb ? "" : " *"}`;
