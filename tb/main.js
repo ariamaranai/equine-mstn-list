8c76a1f868c9512fb42262001a38f817408fca37
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-14 00:08:51 +0900

    Delete tb/main.js

--- tb/main.js
+++
@@ -1,7 +0,0 @@
-addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 40 - document.body.scrollTop % 40 || 40 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 40 || -40), { passive: !1 }),
-ondragstart =n=> !1,
-oninput =n=> {
-  let e = document.body.children, r = $total, t = 0;
-  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 'a', --r);
-  p.textContent = t
-}
