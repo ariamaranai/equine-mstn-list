c2975b142da827319173a4ed05e1a4ac9cd811a2
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-11-04 13:08:10 +0900

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,22 @@
+ondragstart =e=> !1;
+{
+  oninput =e=> {
+    if (a ??= [...t.children], r = $total, l = (e = e.target).selectedIndex) {
+      if (--l < 4)
+        while (t.appendChild(a[$orders[l][r]]), --r);
+      else
+        (location.href = "//ariamaranai.github.io")
+    } else if (l == 0)
+      while (t.prepend(a[r]), --r);
+    else {
+      d ??= a.map(e => e.textContent),
+      e = e.value, l = 0;
+      while (a[r].setAttribute("style", d[r].includes(e) ? (++l, "") : "display:none"), --r);
+      p.textContent = l
+    }
+  },
+  addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
+    e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 40 - t.scrollTop % 40 || 40 : - t.scrollTop % 40 || -40) :
+    e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 40 : -40), { passive: !1 });
+  let t = document.body, l, a, d
+}
