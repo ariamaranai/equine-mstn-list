1b18ee7144b9a866ed4afe5567b1f42834f55f55
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-03 18:47:55 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -9,14 +9,14 @@ ondragstart =e=> !1;
     } else if (l == 0)
       while (t.prepend(a[r]), --r);
     else {
-      d ??= a.map(e => e.textContent),
-      e = e.value.trim().replace(/['.]/g, "").toLowerCase().replace(/(^| )\w/g, e=>e.toUpperCase()), l = 0;
+      d ??= a.map(e => e.textContent.toLowerCase()),
+      e = e.value.trim().replace(/['.]/g, "").toLowerCase(), l = 0;
       while (a[r].setAttribute("style", d[r].includes(e) ? (++l, "") : "display:none"), --r);
       p.textContent = l
     }
   },
   addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
-    e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 40 - t.scrollTop % 40 || 40 : - t.scrollTop % 40 || -40) :
-    e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 40 : -40), { passive: !1 });
-  let t = document.body, l, a, d, o=$orders
+    e => e.preventDefault(scrollBy(0, e.deltaY > 0 ? 40 - t.scrollTop % 40 || 40 : - t.scrollTop % 40 || -40)) :
+    e => e.preventDefault(scrollBy(0, e.deltaY > 0 ? 40 : -40)), { passive: !1 });
+  let l, t = document.body, a, d, o=$orders
 }
