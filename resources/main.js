0ee8143d0397081b7c426cf23bf75932f32fe8a2
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-23 18:02:37 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,22 +1,25 @@
 ondragstart = e => !1;
 {
-  oninput = e => {
-    if (a ??= [...t.children], r = $total, l = (e = e.target).selectedIndex) {
-      if (--l < 3)
+  e.oninput = e => {
+    let r = $total, l = e.target.selectedIndex;
+    if (l > 3)
+      location.href = "//ariamaranai.github.io";
+    else
+      if (a ??= [...t.children], l--)
         while (t.appendChild(a[$orders[l][r]]), --r);
       else
-        location.href = "//ariamaranai.github.io"
-    } else if (l == 0)
-      while (t.prepend(a[r]), --r);
-    else {
-      d ??= a.map(e => e.textContent.toLowerCase()),
-      e = e.value.trim().replace(/['.]/g, "").toLowerCase(), l = 0;
-      while (a[r].setAttribute("style", d[r].includes(e) ? (++l, "") : "display:none"), --r);
-      s.textContent = l
+        while (t.firstChild.after(a[r]), --r);
+  },
+  r.oninput = e => {
+    let r = $total, l = e.target.value.trim().toLowerCase().replace(/[^a-z\u30a1-\u30fc]/g, ""), i = 0;
+    if (l) {
+      d ??= (a ??= [...t.children]).map(e => e.textContent.toLowerCase());
+      while (a[r].setAttribute("style", d[r].includes(l) ? (++i, "") : "display:none"), --r);
+      s.textContent = i
     }
   },
   addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
     e => (e.preventDefault(), scrollBy(0, e.deltaY > 0 ? 40 - t.scrollTop % 40 || 40 : - t.scrollTop % 40 || -40)) :
     e => (e.preventDefault(), scrollBy(0, e.deltaY > 0 ? 40 : -40)), { passive: !1 });
-  let l, t = document.body, a, s = t.firstChild.children[1], d
+  let t = document.body, a, s = t.firstChild.children[1], d
 }
