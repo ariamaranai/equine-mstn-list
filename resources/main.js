dde439e7f919946863215411019bea3036a3c8de
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-23 20:01:12 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -5,22 +5,21 @@ ondragstart = e => !1;
     if (l > 3)
       location.href = "//ariamaranai.github.io";
     else
-      if (a ??= [...t.children], l--)
-        while (t.appendChild(a[$orders[l][r]]), --r);
+      if (n ??= [...t.children], l--)
+        while (t.appendChild(n[$orders[l][r]]), --r);
       else
-        while (t.firstChild.after(a[r]), --r);
+        while (a.after(n[r]), --r);
   },
   r.oninput = e => {
     let r = $total, l = e.target.value.toLowerCase().replace(/[^\u0020a-z\u30a1-\u30fc]/g, ""), i = 0;
-    console.log(l);
     if (l) {
-      d ??= (a ??= [...t.children]).map(e => e.textContent.toLowerCase());
-      while (a[r].setAttribute("style", d[r].includes(l) ? (++i, "") : "display:none"), --r);
+      d ??= (n ??= [...t.children]).map(e => e.textContent.toLowerCase());
+      while (n[r].setAttribute("style", d[r].includes(l) ? (++i, "") : "display:none"), --r);
       s.textContent = i
     }
   },
   addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
     e => (e.preventDefault(), scrollBy(0, e.deltaY > 0 ? 40 - t.scrollTop % 40 || 40 : - t.scrollTop % 40 || -40)) :
     e => (e.preventDefault(), scrollBy(0, e.deltaY > 0 ? 40 : -40)), { passive: !1 });
-  let t = document.body, a, s = t.firstChild.children[1], d
+  let t = document.body, n, s = a.children[1], d
 }
