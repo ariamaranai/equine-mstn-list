37ac7eac5393c568cfeb6faa8b667e6add86343e
a <106607480+ariamaranai@users.noreply.github.com>
2025-05-28 18:29:19 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -25,6 +25,5 @@ ondragstart = e => !1;
     let r = t.scrollTop % 40;
     e.preventDefault(scrollBy(0, e.deltaY > 0 ? r < 1 ? 40 : 40 - r : r < 1 ? -40 : -r));
   }, { passive: !1 });
-  let t = document.body, n = [...t.childNodes], s = r.nextSibling, d = n.map(e => e.textContent);
-  n.shift()
+  let t = document.body, n = [...t.childNodes], s =  (n.shift(), r.nextSibling), d = n.map(e => e.textContent)
 }
