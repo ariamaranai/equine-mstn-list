945c38bc703d66787c5ad3ac8de5c9142134b564
a <106607480+ariamaranai@users.noreply.github.com>
2025-09-22 12:43:31 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -1,80 +1,58 @@
-import horses from "./../../equine-lib/mstn-tb.json";
+import heights from "../../equine-lib/bmi.json";
+import mstns from "../../equine-lib/mstn-tb.json";
 
-let total = horses.length;
-let sort = arr => arr.toSorted((a, b) => a[0] != b[0] && a[0] > b[0] || -1).map(v => v[1]);
-let orderSire = sort(horses.map((v, i) => [v[1].name, i]));
-let orderDam = sort(horses.map((v, i) => [v[2].name, i]));
-let orderDamSire = sort(horses.map((v, i) => [v[3].name, i]));
-let orders = `[[${orderSire}],[${orderDam}],[${orderDamSire}]]`;
-let css = (await Bun.file("main.css").text())
-  .replace(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
+let hrefs = heights.map(v => v.href);
+let toIndexOf = v => hrefs.indexOf(v.href);
+let wts = heights.toSorted((a, b) => a.wt - b.wt || -1).map(toIndexOf);
+let bmis = heights.toSorted((a, b) => a.bmi - b.bmi || -1).map(toIndexOf);
+let years = heights.toSorted((a, b) => b.year - a.year || -1).map(toIndexOf);
+let total = heights.length;
+let orders = `[[${wts}],[${bmis}],[${years}]]`;
+let css = (await Bun.file("main.css").text()).replace(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
 let js = (await Bun.file("main.js").text())
-  .replace(/(?<!let)\s/g, "")
-  .replaceAll("elseif", "else if")
-  .replaceAll("elsewhile", "else while")
+  .replace(/(?<!let|new)\s/g, "")
   .replaceAll("$total", total)
   .replace("$orders", orders)
-  .replace("\\w", "\\w ")
+  .replace("elsewhile", "else while")
+  .replace("elsel", "else l")
+  .replaceAll("elseif", "else if")
+  .replace("\\w", "\\w ");
 let html = (await Bun.file("main.htm").text())
   .replaceAll("\n", "")
   .replaceAll("$total", total)
   .replace("/*css*/", css);
-// let names = horses.map(v => v[0].name);
-for (let i = 0; i < total; ++i) {
-  html += "<p>";
-  for (let j = 0; j < 4; ++j) {
-    let { name, year, cnty, sex, href, mstn } = horses[i][j];
-    /*
-    if (j && mstn)  {
-      names.includes(name) || console.log(name)
-    }*/
-    if (href) {
-      let host = href.slice(8, href.indexOf("/", 10));
-      href = " href=" + (
-        host == "www.pedigreequery.com"
-        ? (/*href.slice(30).replaceAll("+", " ").replace(/[0-9]$/, "").replace(/[0-9]$/, "") != name.toLowerCase() && console.log(name),*/
-          //href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+")
-          href.slice(30)
-          )
-        : host == "www.allbreedpedigree.com"
-          ? "//www.allbreedpedigree.com/" +
-            // href.slice(33).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+") 
-            href.slice(33)
-          : (href.slice(6))
-      );
-    }
-    let mstnAttr = mstn
-      ? mstn == "CC" ? " r"
-      : mstn == "CT" ? " a"
-      : mstn == "TT" ? " e"
-      : mstn[0] == "C" ? " h"
-      : mstn[0] == "T" ? " s"
-      : ""
-      : "";
-    html +=
-      `<a${href}${mstnAttr}>${name.toLowerCase()}\u000a${
-        year 
-          ? cnty
-            ? year + "-" + cnty
-            : year
-          : cnty
-            ? cnty
-            : ""
-        }${
-          j
-            ? ""
-            : sex
-              ? "-" + sex
-              : ""
-        }${
-          (mstn = mstn[1]) >= "0" && mstn <= "9"
-            ? " [" + mstn + "]"
-            : ""
-        }`;
-  }
-  html += "</a>";
+let toMstnAttr = v =>
+  v == "CC" ? " a" :
+  v == "CT" ? " s" :
+  v == "TT" ? " i" : "";
+let _mstns = mstns.map(v => v[0]).filter(v =>
+  v.sex == "S" &&
+  v.name != "-" &&
+  v.mstn == "CC" ||
+  v.mstn == "CT" ||
+  v.mstn == "TT"
+);
+let _mstnNames = _mstns.map(v => v.name);
+let _mstnsSearch = (name, mstn) => {
+  let idx = _mstnNames.indexOf(name);
+  (mstn != (idx >= 0 ? _mstns[idx].mstn : "--")) && console.log(name);
 }
-Bun.write("../.js", js);
-Bun.write("../index.htm", html = html.slice(0, -4));
+
+for (let i = 0; i < heights.length; ++i) {
+  let { name, year, hh, wt, bmi, href, sire, dsire, mstn } = heights[i];
+  html += `<p${toMstnAttr(mstn[0])}><a href=${
+    href[12] == "j"
+      ? href.slice(-11)
+      : "//www.pedigreequery.com/" + href.slice(30)
+  }>${name.toLowerCase()}
+${year}</a><b>${hh} ${wt} ${bmi.toFixed(1)}</b>${sire.toLowerCase()}
+${dsire.toLowerCase()}`;
+  _mstnsSearch(name, mstn[0]);
+  _mstnsSearch(sire, mstn[1]);
+  _mstnsSearch(dsire, mstn[2]);
+}
+
+Bun.write("../s.js", js);
+Bun.write("../index.htm", html);
 console.log(`html size: ${Bun.gzipSync(Buffer.from(html)).length}`);
 console.log(`js size: ${Bun.gzipSync(Buffer.from(js)).length}`);
